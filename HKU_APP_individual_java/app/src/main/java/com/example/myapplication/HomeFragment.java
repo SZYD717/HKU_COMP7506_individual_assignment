e2d396d11447d3a3940e34ae86a027f2a20599bc
SZYD <elg55ban@126.com>
2023-04-26 19:42:36 +0800

    individual_assignment for HKU_COMP7506

--- HKU_APP_individual_java/app/src/main/java/com/example/myapplication/HomeFragment.java
+++ HKU_APP_individual_java/app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -8,13 +8,12 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.myapplication.adapter.HomepageAdapter;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.recycleView);
